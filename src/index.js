5b18bbeeb4916c73044ac31d678634c607412d0d
Rider <goejrider@gmail.com>
2022-11-03 10:04:22 -0500

    setup redux + saga

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import axios from 'axios';
+import createSagaMiddleware from 'redux-saga'
+import {takeEvery, put} from 'redux-saga/effects'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const search_results = (state, action) => {
+
+};
+
+const favorites = (state, action) => {
+
+}
+
+const category = (state, action) => {
+
+}
+
+function* watcherSaga(){
+
+}
+
+createSagaMiddleware(sagaMiddleware);
+
+const storeInstace = createStore(
+    combineReducers({
+        search_results,
+        favorites,
+        category
+    },
+    applyMiddleware(sagaMiddleware, logger))
+)
+
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(<Provider store={storeInstace}><App /></Provider>, document.getElementById('root'));
